28c230d05b72242c239bc01fd2c40d6e5e7ef574
agastyam12 <83354866+agastyam12@users.noreply.github.com>
2021-09-15 18:09:55 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDv9ZYH5z8nQHocbIRorxe5DectW2mJ8Ow",
+  authDomain: "databases-1e33e.firebaseapp.com",
+  databaseURL: "https://databases-1e33e-default-rtdb.firebaseio.com",
+  projectId: "databases-1e33e",
+  storageBucket: "databases-1e33e.appspot.com",
+  messagingSenderId: "7661101917",
+  appId: "1:7661101917:web:0d5d6e733e671426d5beb4",
+  measurementId: "G-7SCB7353CL"
+};
